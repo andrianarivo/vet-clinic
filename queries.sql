6ac499cdc3f7895d9219f3a2fe4f19086cb87dca
Andrianarivo David <andrianarivo.david@gmail.com>
2023-07-18 20:29:33 +0300

    improve query 3

--- queries.sql
+++ queries.sql
@@ -42,3 +42,4 @@ SELECT COUNT(*), species.name AS species_name FROM animals INNER JOIN species ON
 
 SELECT COUNT(*) FROM visits where animal_id = 4;
 SELECT * FROM visits where vet_id = 2;
+SELECT * FROM owners where email = 'owner_18327@mail.com';
